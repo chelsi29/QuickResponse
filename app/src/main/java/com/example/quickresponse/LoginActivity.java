671542e634de78ec2f7b69ef92d88897986c93c6
Ashish Parmar <parmarashish1105@gmail.com>
2022-09-18 12:09:19 +0530

    warning

--- app/src/main/java/com/example/quickresponse/LoginActivity.java
+++ app/src/main/java/com/example/quickresponse/LoginActivity.java
@@ -2,18 +2,31 @@ package com.example.quickresponse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    ImageButton passvisible;
+    EditText mail,pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+
     }
+
+    public void Login(View view) {
+        Intent i = new Intent(LoginActivity.this, WelcomeActivity.class);
+        startActivity(i);
+    }
+
 }
--- app/src/main/java/com/example/quickresponse/WelcomeActivity.java
+++ app/src/main/java/com/example/quickresponse/WelcomeActivity.java
@@ -2,7 +2,9 @@ package com.example.quickresponse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void Login(View view) {
+        Intent i = new Intent(WelcomeActivity.this, ResetActivity.class);
+        startActivity(i);
     }
 }
